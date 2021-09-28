2a72534cbe619462e106b41ed38d73bdb56ffda0
jlafontaine <jlafontaine@student.unimelb.edu.au>
2021-09-29 08:16:10 +1000

    initial commit

---
+++ src/ShadowFlap.java
@@ -0,0 +1,28 @@
+import bagel.*;
+
+/**
+ * @author: James La Fontaine
+ */
+public class ShadowFlap extends AbstractGame {
+
+    public ShadowFlap() {
+    }
+
+    /**
+     * The entry point for the program.
+     */
+    public static void main(String[] args) {
+        ShadowFlap game = new ShadowFlap();
+        game.run();
+    }
+
+    /**
+     * Performs a state update.
+     * allows the game to exit when the escape key is pressed.
+     */
+    @Override
+    public void update(Input input) {
+
+    }
+
+}
